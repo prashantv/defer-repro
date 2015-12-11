fd4cc684c4c6e56824c99e665e3f1bc65ffaac39
Prashant Varanasi <prashant@uber.com>
2015-12-11 14:31:08 -0800

    Simplify repro, throws "bad pointer" panic

--- main.go
+++ main.go
@@ -10,15 +10,11 @@ func main() {
 	for i := 0; i < 1000; i++ {
 		go func() {
 			for {
-				handleRequest()
+				methodWithError()
 			}
 		}()
 	}
 	select {}
-}
-
-func returnsErr() error {
-	return errors.New("[err string]")
 }
 
 func instrument(err *error) func() {
@@ -27,16 +23,7 @@ func instrument(err *error) func() {
 	}
 }
 
-func methodWithError() (retErr error) {
-	defer instrument(&retErr)()
-	if err := returnsErr(); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func handleRequest() {
-	err := methodWithError()
-	fmt.Fprintln(ioutil.Discard, "error", err.Error())
+func methodWithError() (err error) {
+	defer instrument(&err)()
+	return errors.New("error")
 }
